9701af2cae84b779bdcffd70831ce5cf2acc1513
mohamaddarya <mohamaddarya80@gmail.com>
2022-02-12 11:12:54 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("1309660"))
+API_HASH = os.getenv("ba9dcff03638149081fc19693a43d600")
+BOT_TOKEN = os.getenv("5234613039:AAE-9giAYcDboJhcWJL8P8oTay35LQY3Imc")
 DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+OWNER_ID = list({int(x) for x in os.environ.get("592299058", "").split()})
