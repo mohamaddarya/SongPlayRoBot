74510e11cd827df4d0e1d4c631eb0efc36abcef8
mohamaddarya <mohamaddarya80@gmail.com>
2022-02-12 13:05:33 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
-API_HASH = os.getenv("ba9dcff03638149081fc19693a43d600")
+API_ID = int(os.getenv("1311893"))
+API_HASH = os.getenv("9ee1ae5201d2af501d62f9b8f0cbf9b9")
 BOT_TOKEN = os.getenv("5234613039:AAE-9giAYcDboJhcWJL8P8oTay35LQY3Imc")
 DATABASE_URL = os.getenv("https://mamadmamadsong2.herokuapp.com/")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "592299058").split()})
