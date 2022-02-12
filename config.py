ea692eb98fbb2f179da55702aa29112d63e7e240
mohamaddarya <mohamaddarya80@gmail.com>
2022-02-12 15:48:15 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-API_ID = int(os.getenv("1311893"))
+API_ID = int("1311893")
 API_HASH = os.getenv("9ee1ae5201d2af501d62f9b8f0cbf9b9")
 BOT_TOKEN = os.getenv("5234613039:AAE-9giAYcDboJhcWJL8P8oTay35LQY3Imc")
 DATABASE_URL = os.getenv("https://mamadmamadsong2.herokuapp.com/")
